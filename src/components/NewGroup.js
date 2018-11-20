dda89c8daa5b152336edf818ced022839d4d0508
anthonySchafrik <anthonySchafrik@gmail.com>
2018-11-19 20:03:01 -0500

    Add the layout to add a new group

--- src/components/NewGroup.js
+++ src/components/NewGroup.js
@@ -5,36 +5,57 @@ export default class NewGroup extends Component {
     super(props);
 
     this.state = {
-      group: ""
+      groupOptions: "",
+      newGroup: {
+        groupName: "",
+        groupPass: "",
+        groupPassCheck: "",
+        totalPeople: 0,
+        totalCashAmount: 0
+      }
     };
     this.whichGroupToRender = this.whichGroupToRender.bind(this);
-    this.setGroupState = this.setGroupState.bind(this);
+    this.handleGroupState = this.handleGroupState.bind(this);
   }
 
-  setGroupState(event) {
-    this.setState({ group: event.target.value });
+  handleGroupState(event) {
+    this.setState({ groupOptions: event.target.value });
   }
 
   whichGroupToRender() {
-    const { group } = this.state;
+    const { groupOptions } = this.state;
 
-    if (group === "makeNewGroup") {
-      return <div>make new group switch statment works</div>;
+    if (groupOptions === "makeNewGroup") {
+      return (
+        <div>
+          <label>Group Name:</label>
+          <input id="groupName" type="text" />
+          <label>Group Password:</label>
+          <input id="groupPass" type="password" />
+          <label>Group Password Check:</label>
+          <input id="groupPassCheck" type="password" />
+          <label>How many People?:</label>
+          <input id="totalPeople" type="number" />
+          <label>Total Cash Amount</label>
+          <input id="totalCashAmount" type="number" />
+        </div>
+      );
     }
-    if (group === "editGroup") {
+    if (groupOptions === "editGroup") {
       return <div>edit group switch statment works</div>;
     }
   }
 
   render() {
-    const { whichGroupToRender, setGroupState } = this;
+    const { whichGroupToRender, handleGroupState } = this;
+
     return (
       <div>
         <h3>Click button for options you are looking for.</h3>
-        <button onClick={setGroupState} value="makeNewGroup">
+        <button onClick={handleGroupState} value="makeNewGroup">
           Make New Group
         </button>
-        <button onClick={setGroupState} value="editGroup">
+        <button onClick={handleGroupState} value="editGroup">
           Edit Group
         </button>
         {whichGroupToRender()}
