b2f11845256f8159b060b42b18f1309e107a95ad
unknown <anthonyschafrik@gmail.com>
2019-01-19 00:24:12 -0500

    Display all the users that are in the gorup

--- src/components/GroupDetails.js
+++ src/components/GroupDetails.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
 
-import { fetchGroupName } from '../actions';
+import { fetchGroupName } from "../actions";
 
 class GroupDetails extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      groupDetails: [],
+      isLoading: false
+    };
+
     this.handleGroupName = this.handleGroupName.bind(this);
     this.fetchUserGroupInfo = this.fetchUserGroupInfo.bind(this);
+    this.loading = this.loading.bind(this);
+    this.userInfo = this.userInfo.bind(this);
   }
+
   handleGroupName(event) {
     const { value: name } = event.target;
 
@@ -20,27 +28,52 @@ class GroupDetails extends Component {
   fetchUserGroupInfo() {
     const { name: group } = this.props.getGroupName;
 
+    this.setState({ isLoading: true });
+
     axios.get(`/userGroupInfo/?group=${group}`).then(res => {
-      console.log(res);
+      this.setState({ groupDetails: res.data });
+    });
+  }
+
+  loading() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return <h4>Loading...</h4>;
+    }
+  }
+
+  userInfo() {
+    const { groupDetails } = this.state;
+
+    return groupDetails.map(user => {
+      return <p>{user.firstname}</p>;
     });
   }
 
   render() {
-    const { handleGroupName, fetchUserGroupInfo } = this;
-    return (
-      <div>
-        <p>
-          To see how many people have joined your group and there names type in
-          group name below.
-        </p>
-        <input
-          onChange={handleGroupName}
-          type="text"
-          placeholder="Group Name"
-        />
-        <button onClick={fetchUserGroupInfo}>Search</button>
-      </div>
-    );
+    const { handleGroupName, fetchUserGroupInfo, loading, userInfo } = this;
+    const { groupDetails } = this.state;
+    console.log(groupDetails);
+    if (!groupDetails.length) {
+      return (
+        <div>
+          <p>
+            To see how many people have joined your group and there names type
+            in group name below.
+          </p>
+          <input
+            onChange={handleGroupName}
+            type="text"
+            placeholder="Group Name"
+          />
+          <button onClick={fetchUserGroupInfo}>Search</button>
+          {loading()}
+        </div>
+      );
+    } else {
+      return <div>{userInfo()}</div>;
+    }
   }
 }
 
